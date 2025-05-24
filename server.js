fa04fde9ab709369ee34b657098645d201a25179
SudoSantos <twentycodm@outlook.com>
2025-05-24 23:08:08 +0100

    create cookie for user validation

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const express = require("express");
 const db = require("better-sqlite3")("ourApp.db");
@@ -69,9 +71,20 @@ app.post("/register", (req, res) => {
   req.body.password = bcrypt.hashSync(req.body.password, salt)
   
   const ourStatement = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)")
-  ourStatement.run(req.body.username, req.body.password)
+  const result = ourStatement.run(req.body.username, req.body.password)
+
+  const lookupStatement = db.prepare("SELECT * FROM users WHERE ROWID = ?")
+  const ourUser = lookupStatement.get(result.lastInsertRowid)
 
   // 2. log the user in by giving them a cookie
+  const ourTokenValue = jwt.sign({exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24, skyColor: "blue", userid: ourUser.id, username: ourUser.username}, process.env.JWTSECRET)
+
+  res.cookie("ourSimpleApp", "supertopsecretvalue", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 1000 * 60 * 60 * 24
+  })
 
   res.send("Thank you!")
 });
