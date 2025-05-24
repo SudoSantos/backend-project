8700bf5fd8cf0a9b47b8a897067f8c8a34bdcebb
SudoSantos <twentycodm@outlook.com>
2025-05-24 22:35:25 +0100

    function to save new user into the database and hash user password

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
 const db = require("better-sqlite3")("ourApp.db");
-db.pragma("journal_mode = WAL")
+db.pragma("journal_mode = WAL");
+
+//database setup here
+const createTables = db.transaction(() => {
+  db.prepare(
+    `
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username STRING NOT NULL UNIQUE,
+    password STRING NOT NULL
+    )
+    `
+  ).run();
+});
+createTables();
+
+//database setup ends here
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   res.locals.errors = [];
-  next()
-})
+  next();
+});
 
 app.get("/", (req, res) => {
   res.render("homepage");
@@ -22,6 +39,7 @@ app.get("/login", (req, res) => {
 
 app.post("/register", (req, res) => {
   const errors = [];
+
   if (typeof req.body.username !== "string") req.body.username = "";
   if (typeof req.body.password !== "string") req.body.password = "";
 
@@ -32,23 +50,30 @@ app.post("/register", (req, res) => {
     errors.push("Username must be at least 3 characters.");
   if (req.body.username.length && req.body.username.length > 10)
     errors.push("Username should not be more than 10 characters.");
-  if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push("Usernames can only contain letters and numbers");
-
+  if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/))
+    errors.push("Usernames can only contain letters and numbers");
 
   if (!req.body.password) errors.push("You must provide a password.");
-  if (req.body.password.lengthreq.body.password.length < 6)
+  if (req.body.password.length && req.body.password.length < 6)
     errors.push("Password must be at least 6 characters.");
   if (req.body.password.length && req.body.password.length > 20)
     errors.push("Password should not exceed 20 characters.");
 
-  if (errors.length){
-    return res.render("homepage", {errors})
+  if (errors.length) {
+    return res.render("homepage", { errors });
   }
 
   //After successful user registration
   // 1. save the new user into a database
+  const salt = bcrypt.genSaltSync(10);
+  req.body.password = bcrypt.hashSync(req.body.password, salt)
+  
+  const ourStatement = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)")
+  ourStatement.run(req.body.username, req.body.password)
 
   // 2. log the user in by giving them a cookie
+
+  res.send("Thank you!")
 });
 
 app.listen(3000);
